9a521ee21e7574393caef1560b870aef6b23248b
Eli Lauwers <eli.lauwers@be-mobile.com>
2022-02-02 09:17:52 +0100

    class update

--- main.py
+++ main.py
@@ -1,66 +1,91 @@
 
 class Board:
-    def __init__(self, data):
-        if isinstance(data, int):
-            self.data = self.create_lights(data)
-        elif isinstance(data, list):
-            assert all([len(el) == len(data) for el in data]), "Not a square"
-            self.data = data
-        else:
-            raise AssertionError("invalid board")
+    def __init__(self, dim, points = None):
+        self.dim = dim
+        # if no points were supplied, create new points
+        points = self.create_points() if not points else points
+        # check if no point lays outside of the board
+        points_check = [all([el <= dim - 1 for el in point]) for point in points]
+        assert all(points_check), "Points exceed dimension"
+        self.points = set(points)
 
     def __repr__(self):
-        str = f"Board({self.data})"
+        str = f"Board({self.dim}, points={self.points})"
         return str
 
     def __str__(self):
-        return "\n".join([" ".join(row) for row in self.data])
+        """
+        Creates a matrix representation of the board
+        """
+        matrix = []
+        for i in range(self.dim):
+            row = []
+            for j in range(self.dim):
+                val = "X" if (i, j) in self.points else "O"
+                row.append(val)
+            matrix.append(" ".join(row))
+        return "\n".join(matrix)
 
-    def print_lights(self, array, raw = True):
-        dim = len(array)
+    def print_lights(self, raw = True):
+        """
+        Pivots the matrix 45 degrees and prints
+        :param raw: should original coords be printed (False) or raw points (True)
+        :return: string representation of the board
+        """
         # create lens out of the dims
-        lens = [dim]
-        for i in range(dim - 1, 0, -1):
+        lens = [self.dim]
+        for i in range(self.dim - 1, 0, -1):
             lens.append(i)
             lens.insert(0, i)
+
+        # points to array
+        array = []
+        for i in range(self.dim):
+            row = []
+            for j in range(self.dim):
+                if raw:
+                    val = "X" if (i, j) in self.points else "O"
+                else:
+                    val = f"{i}.{j}"
+                row.append(val)
+            array.append(row)
+
         # create the printed matrix
+        betw_elements = 3 * ' ' if raw else 5 * ' '
+        center = 4 * self.dim + 2 if raw else 7 * self.dim + 2
         new = []
         for length in lens:
             new_row = []
             for ri in range(length):
                 ri = length - ri - 1
                 val = array[ri][0]
-                if not raw:
-                    val = "X" if val is not None else "0"
                 new_row.append(val)
                 del array[ri][0]
                 if array[ri] == []:
                     del array[ri]
-            new.append(new_row)
+            new.append(f"{betw_elements.join(new_row).center(center)}")
 
-        return "\n".join([f"{(3 * ' ').join(row).center(4 * dim + 2)}" for row in new])
+        return "\n".join([row for row in new])
+
+    def create_points(self):
+        """
+        Used when no points are supplied
+        :return: a list of points needed in the init
+        """
+        # create a half empty matrix
+        locs = [(i, j) for i in range(self.dim) for j in range(self.dim)]
+        points = []
+        for loc in locs:
+            if sum(loc) > (self.dim - 1):
+                points.append(loc)
+        return points
 
 
-    def create_coords(self, dim):
-        data = []
-        for i in range(dim):
-            row = []
-            for j in range(dim):
-                row.append(f"{i}.{j}")
-            data.append(row)
-        return data
-
-    def create_lights(self, dim):
-        data = []
-        for i in range(dim):
-            row = []
-            for j in range(dim):
-                row.append(f"{i}.{j}")
-            data.append(row)
-        return [["X" if (row + col) >= dim else "O" for col in range(dim)] for row in range(dim)]
 
 
 
 if __name__ == "__main__":
-    board = Board(8)
+    board = Board(4)
     print(board)
+    print(board.__repr__())
+    print(board.print_lights(raw = False))
