2dd451d9c170aec3b78b3e689b8c0cd476d264c5
Eli Lauwers <eli.lauwers@be-mobile.com>
2022-02-02 14:18:45 +0100

    neighbors

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import re
 
 class Board:
     def __init__(self, dim, points = None):
@@ -10,6 +11,10 @@ class Board:
         self.points = set(points)
 
     def __repr__(self):
+        """
+        Create a string representation of the matrix
+        :return: string
+        """
         str = f"Board({self.dim}, points={self.points})"
         return str
 
@@ -18,10 +23,10 @@ class Board:
         Creates a matrix representation of the board
         """
         matrix = []
-        for i in range(self.dim):
+        for x in range(self.dim):
             row = []
-            for j in range(self.dim):
-                val = "X" if (i, j) in self.points else "O"
+            for y in range(self.dim):
+                val = "X" if (x, y) in self.points else "O"
                 row.append(val)
             matrix.append(" ".join(row))
         return "\n".join(matrix)
@@ -40,13 +45,13 @@ class Board:
 
         # points to array
         array = []
-        for i in range(self.dim):
+        for x in range(self.dim):
             row = []
-            for j in range(self.dim):
+            for y in range(self.dim):
                 if raw:
-                    val = "X" if (i, j) in self.points else "O"
+                    val = "X" if (x, y) in self.points else "O"
                 else:
-                    val = f"{i}.{j}"
+                    val = f"{x}.{y}"
                 row.append(val)
             array.append(row)
 
@@ -73,19 +78,81 @@ class Board:
         :return: a list of points needed in the init
         """
         # create a half empty matrix
-        locs = [(i, j) for i in range(self.dim) for j in range(self.dim)]
+        locs = [(x, y) for x in range(self.dim) for y in range(self.dim)]
         points = []
         for loc in locs:
             if sum(loc) > (self.dim - 1):
                 points.append(loc)
         return points
 
+    def neighbor(self, point, direction):
+        """
+        Get the neighbor of a point in a given direction. Checks whether the point can be occupied
+        :param point:
+        :param direction:
+        :return:
+        """
+        y, x = point
+
+        if "U" in direction:
+            y -= 1
+        elif "D" in direction:
+            y += 1
+
+        if "L" in direction:
+            x -= 1
+        elif "R" in direction:
+            x += 1
+
+        # points must fall in the board
+        if not 0 <= x < self.dim:
+            return None
+        if not 0 <= y < self.dim:
+            return None
+        return y, x
+
+    def update(self, gravity = "DR"):
+        """
+        Moves all points in given direction
+        """
+        assert gravity in ["U", "D", "L", "R", "UR", "DR", "DL", "UL"], "Invalid gravity"
+        point_found = True
+        while point_found:
+            point_found = False
+            for point in self.points:
+                # get the point in the wanted direction
+                quest = self.neighbor(point, direction = gravity)
+
+                if not quest:
+                    continue
+
+                neighbor_empty = quest not in self.points
+                if neighbor_empty:
+                    point_found = True
+                    self.points.remove(point)
+                    self.points.add(quest)
+            x = 1
+        return self
+
+
+
+
 
 
 
 
 if __name__ == "__main__":
+
     board = Board(4)
-    print(board)
-    print(board.__repr__())
-    print(board.print_lights(raw = False))
+    # print(board)
+    # print(board.print_lights())
+
+    for dir in ["U", "UR",  "R", "DR", "D", "DL", "L", "UL"]:
+        print("Original")
+        print(Board(4))
+        print(f"Dir: {dir}")
+        print(Board(4).update(dir))
+        print("\n\n")
+    # print("")
+    # print(board)
+    # print(board.print_lights())
