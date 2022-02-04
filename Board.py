c80f2d7a2ff28fab43026209df81fdb69232c6de
Eli Lauwers <eli.lauwers@be-mobile.com>
2022-02-04 08:38:01 +0100

    Fix immutible list objects

---
+++ Board.py
@@ -0,0 +1,162 @@
+import random
+
+class Board:
+    def __init__(self, dim, points = None):
+        self.dim = dim
+        # if no points were supplied, create new points
+        points = self.create_points() if not points else points
+        # check if no point lays outside of the board
+        points_check = [all([el <= dim - 1 for el in point]) for point in points]
+        assert all(points_check), "Points exceed dimension"
+        self.points = points
+
+    def __repr__(self):
+        """
+        Create a string representation of the matrix
+        :return: string
+        """
+        str = f"Board({self.dim}, points={self.points})"
+        return str
+
+    def __str__(self):
+        """
+        Creates a matrix representation of the board
+        """
+        matrix = []
+        for x in range(self.dim):
+            row = []
+            for y in range(self.dim):
+                val = "X" if (x, y) in self.points else "-"
+                row.append(val)
+            matrix.append(" ".join(row))
+        return "\n".join(matrix)
+
+    def print_lights(self, raw = True):
+        """
+        Pivots the matrix 45 degrees and prints
+        :param raw: should original coords be printed (False) or raw points (True)
+        :return: string representation of the board
+        """
+        # create lens out of the dims
+        lens = [self.dim]
+        for i in range(self.dim - 1, 0, -1):
+            lens.append(i)
+            lens.insert(0, i)
+
+        # points to array
+        array = []
+        for x in range(self.dim):
+            row = []
+            for y in range(self.dim):
+                if raw:
+                    val = "X" if (x, y) in self.points else "-"
+                else:
+                    val = f"{x}.{y}"
+                row.append(val)
+            array.append(row)
+
+        # create the printed matrix
+        betw_elements = 3 * ' ' if raw else 5 * ' '
+        center = 4 * self.dim + 2 if raw else 7 * self.dim + 2
+        new = []
+        for length in lens:
+            new_row = []
+            for ri in range(length):
+                ri = length - ri - 1
+                val = array[ri][0]
+                new_row.append(val)
+                del array[ri][0]
+                if array[ri] == []:
+                    del array[ri]
+            new.append(f"{betw_elements.join(new_row).center(center)}")
+
+        return "\n".join([row for row in new])
+
+    def create_points(self):
+        """
+        Used when no points are supplied
+        :return: a list of points needed in the init
+        """
+        # create a half empty matrix
+        locs = [(x, y) for x in range(self.dim) for y in range(self.dim)]
+        points = []
+        for loc in locs:
+            if sum(loc) > (self.dim - 1):
+                points.append(loc)
+        return points
+
+    def neighbor(self, point, direction):
+        """
+        Get the neighbor of a point in a given direction. Checks whether the point can be occupied
+        :param point:
+        :param direction:
+        :return:
+        """
+        col, row = point
+
+        if "U" in direction:
+            col -= 1
+        elif "D" in direction:
+            col += 1
+
+        if "R" in direction:
+            row += 1
+        elif "L" in direction:
+            row -= 1
+
+        row_inrange = 0 <= row < self.dim
+        col_inrange = 0 <= col < self.dim
+        if not all([row_inrange, col_inrange]):
+            return None
+        if (col, row) in self.points:
+            return None
+        return (col, row)
+
+    def follow(self, gravity = "DR"):
+        assert gravity in ["U", "D", "L", "R", "UL", "UR", "DR", "DL"], "Invalid gravity"
+        point = self.points[0]
+        # check if the point can fall down to gravity
+        gravpoint = self.neighbor(point, gravity)
+
+        if gravpoint:
+            self.points.remove(point)
+            self.points.append(gravpoint)
+            follow_state = True
+            return self, follow_state
+
+        # the point cannot fall down
+        # let the point fall to a side
+        side_dirs = self.side_directions(gravity)
+        side_points = []
+        for dir in side_dirs:
+            p = self.neighbor(point, dir)
+            if p:
+                side_points.append(p)
+
+        if side_points != []:
+            self.points.remove(point)
+            self.points.append(random.choice(side_points))
+            follow_state = True
+            return self, follow_state
+
+        # nothing happend, put the point back
+        self.points.remove(point)
+        self.points.append(point)
+        return self, False
+
+    def side_directions(self, direction):
+        """
+        Returns the both wanted sides
+        :param direction:
+        :return: list
+        """
+        # if a combined dir has been provided, return the two main dirs
+        if len(direction) == 2:
+            return list(direction)
+        # else, we need to find the sides of a main dir
+        horizontal = ["L", "R"]
+        vertical = ["U", "D"]
+        if direction in horizontal:
+            return [f"{v}{direction}" for v in vertical]
+        return [f"{direction}{h}" for h in horizontal]
+
--- main.py
+++ main.py
@@ -1,170 +1,66 @@
-import random
-import time
-from numpy.random import choice
+from Board import Board
 
-class Board:
-    def __init__(self, dim, points = None):
+class Hourglass:
+    def __init__(self, dim, points = [None, None]):
+        assert len(points) == 2, "Points must be of len 2"
+        p1, p2 = points
         self.dim = dim
-        # if no points were supplied, create new points
-        points = self.create_points() if not points else points
-        # check if no point lays outside of the board
-        points_check = [all([el <= dim - 1 for el in point]) for point in points]
-        assert all(points_check), "Points exceed dimension"
-        self.points = points
+        self.top = Board(self.dim, p1)
+        self.bottom = Board(self.dim, p2)
+        self.gravity = "U"
+        self.initial_gravity = "U"
+
+    def update_gravity(self, gravity):
+        assert gravity in ["U", "D", "L", "R", "UL", "UR", "DR", "DL"], "Invalid gravity"
+        self.gravity = gravity
+        dirs = ["U", "D", "L", "R", "UL", "UR", "DR", "DL"]
+        arrows = ["↑", "↓", "←", "→", "↖", "↗", "↘", "↙"]
+        arrows = {dir : arrow for dir, arrow in zip(dirs, arrows)}
+        return arrows[gravity] * self.dim * 2
+
+
+    def follow(self, gravity):
+        follow_states = []
+        for board in [self.top, self.bottom]:
+            something, follow_state = board.follow(gravity)
+            follow_states.append(follow_state)
+        return self, follow_states
+    def __str__(self):
+        resp = [str(board) for board in [self.top, self.bottom]]
+        return ("\n" + "~" * self.dim * 2 + "\n").join(resp)
 
     def __repr__(self):
+        points = [board.points for board in [self.top, self.bottom]]
+        string = f"Hourglass({self.dim}, points = {points})"
+        return string
+
+    def drop_sand(self):
         """
-        Create a string representation of the matrix
-        :return: string
+        Takes a sand drop from the top board and puts it at the bottom
+        :return: None
         """
-        str = f"Board({self.dim}, points={self.points})"
-        return str
-
-    def __str__(self):
-        """
-        Creates a matrix representation of the board
-        """
-        matrix = []
-        for x in range(self.dim):
-            row = []
-            for y in range(self.dim):
-                val = "X" if (x, y) in self.points else "-"
-                row.append(val)
-            matrix.append(" ".join(row))
-        return "\n".join(matrix)
-
-    def print_lights(self, raw = True):
-        """
-        Pivots the matrix 45 degrees and prints
-        :param raw: should original coords be printed (False) or raw points (True)
-        :return: string representation of the board
-        """
-        # create lens out of the dims
-        lens = [self.dim]
-        for i in range(self.dim - 1, 0, -1):
-            lens.append(i)
-            lens.insert(0, i)
-
-        # points to array
-        array = []
-        for x in range(self.dim):
-            row = []
-            for y in range(self.dim):
-                if raw:
-                    val = "X" if (x, y) in self.points else "-"
-                else:
-                    val = f"{x}.{y}"
-                row.append(val)
-            array.append(row)
-
-        # create the printed matrix
-        betw_elements = 3 * ' ' if raw else 5 * ' '
-        center = 4 * self.dim + 2 if raw else 7 * self.dim + 2
-        new = []
-        for length in lens:
-            new_row = []
-            for ri in range(length):
-                ri = length - ri - 1
-                val = array[ri][0]
-                new_row.append(val)
-                del array[ri][0]
-                if array[ri] == []:
-                    del array[ri]
-            new.append(f"{betw_elements.join(new_row).center(center)}")
-
-        return "\n".join([row for row in new])
-
-    def create_points(self):
-        """
-        Used when no points are supplied
-        :return: a list of points needed in the init
-        """
-        # create a half empty matrix
-        locs = [(x, y) for x in range(self.dim) for y in range(self.dim)]
-        points = []
-        for loc in locs:
-            if sum(loc) > (self.dim - 1):
-                points.append(loc)
-        return points
-
-    def neighbor(self, point, direction):
-        """
-        Get the neighbor of a point in a given direction. Checks whether the point can be occupied
-        :param point:
-        :param direction:
-        :return:
-        """
-        col, row = point
-
-        if "U" in direction:
-            col -= 1
-        elif "D" in direction:
-            col += 1
-
-        if "R" in direction:
-            row += 1
-        elif "L" in direction:
-            row -= 1
-
-        row_inrange = 0 <= row < self.dim
-        col_inrange = 0 <= col < self.dim
-        if not all([row_inrange, col_inrange]):
-            return None
-        if (col, row) in self.points:
-            return None
-        return (col, row)
-
-    def follow(self, gravity = "DR"):
-        assert gravity in ["U", "D", "L", "R", "UL", "UR", "DR", "DL"], "Invalid gravity"
-        point = self.points[0]
-        # check if the point can fall down to gravity
-        gravpoint = self.neighbor(point, gravity)
-        if gravpoint:
-            self.points.remove(point)
-            self.points.append(gravpoint)
-            return self
-        # the point cannot fall down
-        # let the point fall to a side
-        side_dirs = side_directions(gravity)
-        side_points = []
-        for dir in side_dirs:
-            p = self.neighbor(point, dir)
-            if not p:
-                continue
-            side_points.append(p)
-        if side_points != []:
-            self.points.remove(point)
-            self.points.append(random.choice(side_points))
+        # get top board
+        assert self.gravity in ["U","D"], "invalid gravity"
+        if self.gravity == self.initial_gravity:
+            top, bottom = self.top, self.bottom
         else:
-            # nothing happend, put the point back
-            self.points.remove(point)
-            self.points.append(point)
-
-        return self
-
-
-def side_directions(direction):
-    """
-    Returns the both wanted sides
-    :param direction:
-    :return: list
-    """
-    # if a combined dir has been provided, return the two main dirs
-    if len(direction) == 2:
-        return list(direction)
-    # else, we need to find the sides of a main dir
-    horizontal = ["L", "R"]
-    vertical = ["U","D"]
-    if direction in horizontal:
-        return [f"{v}{direction}" for v in vertical]
-    return[f"{direction}{h}" for h in horizontal]
-
-
-
-
+            top, bottom = self.bottom, self.top
+        # check if any sand can drop
+        if top.points == []:
+            # the top board is empty
+            return
+        # Sample one of the top points, where the sum is smallest
+        sums = [sum(p) for p in top.points]
+        index = sums.index(min(sums))
+        poi = top.points[index]
+        # remove that point from the top board and insert it in the bottom board
+        top.points.remove(poi)
+        bottom.points.insert(0, (0,0))
+        return
 
 if __name__ == "__main__":
     from math import ceil
+    import time
     dim = 6
     el = ceil(dim / 2)
     points = [
@@ -172,16 +68,49 @@ if __name__ == "__main__":
         (el - 1, el),
         (el, el - 1)
     ]
-    board = Board(dim, points)
-    dirs = ["U", "D", "L", "R", "UL", "UR","DR","DL"]
-    arrows = ["↑","↓","←","→","↖","↗","↘","↙"]
-    gravity = dirs[0]
-    while True:
-        sides = side_directions(gravity)
-        # probs = [.5 if dir == gravity else .2 if dir in sides else .10 / 5 for dir in dirs]
-        gravity = choice(dirs[:4], 1)
-        for i in range(10):
-            print(arrows[dirs.index(gravity)] * dim * 2)
-            print(board.follow(gravity))
-            time.sleep(.75)
+    hourglass = Hourglass(dim, points=[points, points])
+    dirs = ["U", "D", "L", "R", "UL", "UR", "DR", "DL"]
+    try:
+
+        last_grav = dirs[0]
+        last_follow = False
+        last_update = None
+        update_time = time.time()
+        while True:
+            # update in increments of .2 seconds
+            if time.time() < update_time + .2:
+                continue
+
+            # update gravity for the hourglass
+            # gravity = random.choice(dirs)
+            gravity = "D"
+            print(hourglass.update_gravity(gravity))
+            # check if any point can follow
+            glass, follow_states = hourglass.follow(gravity)
+            print(hourglass)
+            # if any point can follow, we are in the follow state
+            if any(follow_states):
+                last_update = None
+                # there was a follow, no sand will be dropped
+                continue
+
+            # sand can only be dropped when the board is upside down
+            if gravity not in ["D", "U"]:
+                continue
+
+            # there wasn't a follow => drop state
+            if not last_update:
+                last_update = time.time()
+                continue
+
+            # only drop sand in half a second increments
+            if time.time() < last_update + 1:
+                continue
+
+            hourglass.drop_sand()
+
+
+    except KeyboardInterrupt:
+        print("Loop was stopped")
+        pass
 
